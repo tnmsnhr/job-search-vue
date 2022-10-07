169df8709a2f2c8dae152d573530479c8be213a8
tnmsnhr <tnmsnhr@gmail.com>
2022-10-07 17:36:16 +0530

    Adding MainNav Items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,6 +5,20 @@
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-grey-1"
       >
         <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+              data-test="main-nav-list-item"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +30,14 @@ export default {
   data() {
     return {
       company: "J-Board",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at jBoard",
+        "How we hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,20 @@ describe("ManNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("J-Board");
   });
+
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at jBoard",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
